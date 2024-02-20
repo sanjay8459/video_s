14d45bb62c86c1437f7bc7da27c683852d05b313
sanjay8459 <sanjaygss63@gmail.com>
2024-02-20 14:29:25 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ from flask import Flask,request,render_template,redirect,url_for,flash
 
 app = Flask(__name__)
 
-print("cv2 version",cv2.__version__)
+#print("cv2 version",cv2.__version__)
 
 
-def fetch():
-  video =cv2.VideoCapture(0)
-  facesdetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
-  while True:
+#def fetch():
+ 
+  
+@app.route('/')
+def index():
+   video =cv2.VideoCapture(0)
+   facesdetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+   while True:
     ret,frame=video.read()
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces=facesdetect.detectMultiScale(gray,1.3 ,5)
@@ -23,14 +27,10 @@ def fetch():
        break
     
        
-  video.release()
-  cv2.destroyAllWindows()
-  
-@app.route('/')
-def index():
-  
-  fetch()
-  return render_template('index.html')
+   video.release()
+   cv2.destroyAllWindows()
+  #fetch()
+   return render_template('index.html')
 
 
 if __name__=='__main__':
